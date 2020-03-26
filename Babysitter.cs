2032c6f8096cb94385354a167334f424bcff2cb6
Oszkar Nemeth <oszkar.nemeth@outlook.com>
2020-03-26 18:32:53 +0200

    All tests has been implemented.

--- Babysitter.cs
+++ Babysitter.cs
@@ -8,50 +8,60 @@ namespace Babysitter
 {
     public class BabySitter
     {
+        byte[ , ] RateMatrix = new byte[3, 11] 
+            {/* Time frame  5....6....7....8....9....10....11....12....1....2....3....4 */
+             /* Family A */ { 15,  15,  15,  15,  15,   15,   20,   20,  20,  20,  20},
+             /* Family B */ { 12,  12,  12,  12,  12,    8,    8,   16,  16,  16,  16},
+             /* Family C */ { 21,  21,  21,  21,  15,   15,   15,   15,  15,  15,  15},
+            };
+
         static void Main(string[] args)
         {
-        }
-
-        // Method   :   CalculateTotalPay
-        //
-        // Descr.   :   
-        //              
-        //              
-        //              
-        //              
-        // Input    :   string  Family - family type can be A, B or C
-        //              byte    StartHour - exact hour when the babysitter starts
-        //              byte    EndHour - exact hour when the babysitter finishes
-        //
-        // Output   :   string  TotalPay
+        }  
         
-        
-        private bool ValidateFamilyIndex(string Family)
+        private bool ValidateFamilyIndex(char Family)
         {
-            if (Family != "A" && Family != "B" && Family != "C") return false;
+            if (Family != 'A' && Family != 'B' && Family != 'C') return false;
             else return true;
         }
 
-        private bool ValidateStartEndTime(byte StartHour, byte EndHour)
+        private bool ValidateStartEndTime(byte StartIdx, byte EndIdx)
         {
-            if ((StartHour > 11) || (EndHour > 11) || (StartHour >= EndHour)) return false;
+            if ((StartIdx > 11) || (EndIdx > 11) || (StartIdx >= EndIdx)) return false;
             else return true;
         }
 
-        public string CalculateTotalPay(string Family, byte StartHour, byte EndHour)
+        private int CalculatePayByFamilyAndRates(byte FamiliyIdx, byte StartIdx, byte EndIdx)
         {
-            string TotalPay = "0.00 $";
+            int TotalPayment = 0;
+            byte RateIdx = StartIdx;
+
+            while  (RateIdx < EndIdx) TotalPayment += RateMatrix[FamiliyIdx, RateIdx++];
+
+            return TotalPayment;
+        }
+
+        private byte ConvertTimeToIndex(byte Time)
+        {
+            if (Time >= 5 && Time <= 12) return (byte)(Time - 5);
+            else if (Time >= 1 && Time <= 4) return (byte)(Time + 7);
+            else return 255;
+        }
+
+        public string CalculateTotalPay(char Family, byte StartHour, byte EndHour)
+        {
+            byte StartIdx = ConvertTimeToIndex(StartHour);
+            byte EndIdx = ConvertTimeToIndex(EndHour);
 
             if (!ValidateFamilyIndex(Family))
             {
                 return "Error: Incorrect family index!";
             }
-            if (!ValidateStartEndTime(StartHour, EndHour))
+            if (!ValidateStartEndTime(StartIdx, EndIdx))
             {
                 return "Error: Invalid start or end hour!";
             }
-
-            return TotalPay;
+            return CalculatePayByFamilyAndRates((byte)(Family - 'A'), StartIdx, EndIdx).ToString() + ".00 $";
         }
     }
 }
--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -16,34 +16,75 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void NoHoursWorkedNoPayment()
-        {
-            Assert.AreEqual("0.00 $", BabySitterObj.CalculateTotalPay("A", 0, 1));
-        }
-
-        [TestMethod]
         public void IncorrectFamilyIndex()
         {
-            Assert.AreEqual("Error: Incorrect family index!", BabySitterObj.CalculateTotalPay("X", 0, 0));
+            Assert.AreEqual("Error: Incorrect family index!", BabySitterObj.CalculateTotalPay('X', 0, 0));
         }
 
         [TestMethod]
         public void InvalidStartTime()
         {
-            Assert.AreEqual("Error: Invalid start or end hour!", BabySitterObj.CalculateTotalPay("A", 12, 0));
+            Assert.AreEqual("Error: Invalid start or end hour!", BabySitterObj.CalculateTotalPay('A', 13, 4));
         }
 
         [TestMethod]
         public void InvalidEndTime()
         {
-            Assert.AreEqual("Error: Invalid start or end hour!", BabySitterObj.CalculateTotalPay("A", 0, 12));
+            Assert.AreEqual("Error: Invalid start or end hour!", BabySitterObj.CalculateTotalPay('A', 5, 17));
         }
 
         [TestMethod]
         public void StartTimeIsAfterEndTime()
         {
-            Assert.AreEqual("Error: Invalid start or end hour!", BabySitterObj.CalculateTotalPay("A", 10, 9));
+            Assert.AreEqual("Error: Invalid start or end hour!", BabySitterObj.CalculateTotalPay('A', 1, 5));
         }
 
+        [TestMethod]
+        public void WorkedForFamilyA2HoursFrom5PMto7PM()
+        {
+            Assert.AreEqual("30.00 $", BabySitterObj.CalculateTotalPay('A', 5, 7));
+        }
+
+        [TestMethod]
+        public void WorkedForFamilyA4HoursFrom12AMto4AM()
+        {
+            Assert.AreEqual("80.00 $", BabySitterObj.CalculateTotalPay('A', 12, 4));
+        }
+
+        [TestMethod]
+        public void WorkedForFamilyA4HoursFrom9PMto1AM()
+        {
+            Assert.AreEqual("70.00 $", BabySitterObj.CalculateTotalPay('A', 9, 1));
+        }
+
+        [TestMethod]
+        public void WorkedForFamilyB5HoursFrom5PMTo10PM()
+        {
+            Assert.AreEqual("60.00 $", BabySitterObj.CalculateTotalPay('B', 5, 10));
+        }
+
+        [TestMethod]
+        public void WorkedForFamilyB2HoursFrom10PMTo12AM()
+        {
+            Assert.AreEqual("16.00 $", BabySitterObj.CalculateTotalPay('B', 10, 12));
+        }
+
+        [TestMethod]
+        public void WorkedForFamilyB4HoursFrom12AMTo4AM()
+        {
+            Assert.AreEqual("64.00 $", BabySitterObj.CalculateTotalPay('B', 12, 4));
+        }
+
+        [TestMethod]
+        public void WorkedForFamilyB4HoursFrom9PMTo1AM()
+        {
+            Assert.AreEqual("44.00 $", BabySitterObj.CalculateTotalPay('B', 9, 1));
+        }
+
+        [TestMethod]
+        public void WorkedForFamilyC4HoursFrom7PMto11PM()
+        {
+            Assert.AreEqual("72.00 $", BabySitterObj.CalculateTotalPay('C', 7, 11));
+        }
     }
 }
